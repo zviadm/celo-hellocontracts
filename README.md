cf61a988856b44d8b5f6f04dff85e2dfc4afab1a
Zviad Metreveli <thezviad@gmail.com>
2020-11-13 08:40:22 -0500

    better testing practices and more testing documentation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Example smart contract that integrates with the Celo core contracts. Comes with an end-2-end test too, to demonstrate
 how to write tests utilizing @celo/contractkit for more complex interactions.
 
-HelloContract implements following:
+HelloContract implements the following:
 * Allows any user to lock CELO through this contract.
 * Allows any other user to unlock and withdraw locked CELO held through this contract.
 
@@ -21,3 +21,23 @@ Run migrations and run tests:
 > npm run migrate
 > npm test
 ```
+
+## Structure
+
+As described in Truffle documentation: https://www.trufflesuite.com/docs/truffle/testing/testing-your-contracts,
+Truffle provides clean-room environment for each separate test file. However, this clean-room environment only applies
+to contracts that you write and deploy using Truffle. State of the Celo core contracts won't be reset between runs.
+
+We recommend these practices to keep your tests self-contained:
+* Treat each test file as a single large test. Do not put unrelated tests in the same file.
+* Tests within a file run in the order of definition, and it's ok to depend on this order since
+we are treating each file as a single large test.
+* Configure Truffle to run with --bail option to fail the whole file when one test fails.
+* In each test file, create new accounts to use for testing using `web3.eth.personal` APIs, instead of
+using the supplied `accounts` directly. This will make it easier to avoid any core contract state conflicts
+between test runs.
+```
+	const addr = await web3.eth.personal.newAccount("")
+	await web3.eth.personal.unlockAccount(addr, "", 0)
+```
+
--- test-ts/test-hellocontract.ts
+++ test-ts/test-hellocontract.ts
@@ -1,56 +1,81 @@
 import { newKit } from "@celo/contractkit"
 import { Provider, increaseTime } from "celo-devchain"
+import { toWei } from "web3-utils"
 
 const HelloContract = artifacts.require("HelloContract");
 
+const kit = newKit("http://127.0.0.1:7545")
+after(() => { kit.stop() })
 contract('HelloContract', (accounts) => {
-	it(`locking and unlocking`, async () => {
-		const kit = newKit("http://127.0.0.1:7545")
+	const source = accounts[0]
+	let a0: string
+	let a1: string
+	let a2: string
+
+	it(`create accounts`, async () => {
 		const goldToken = await kit.contracts.getGoldToken()
-		const lockedGold = await kit.contracts.getLockedGold()
-		const a0 = accounts[0]
-		const a1 = accounts[1]
-		const a2 = accounts[2]
+		a0 = await web3.eth.personal.newAccount("")
+		await web3.eth.personal.unlockAccount(a0, "", 0)
+		a1 = await web3.eth.personal.newAccount("")
+		await web3.eth.personal.unlockAccount(a1, "", 0)
+		a2 = await web3.eth.personal.newAccount("")
+		await web3.eth.personal.unlockAccount(a2, "", 0)
 
-		const balanceA0 = await goldToken.balanceOf(a0)
-		const balanceA1 = await goldToken.balanceOf(a1)
-		const balanceA2 = await goldToken.balanceOf(a2)
+		await goldToken
+			.transfer(a0, toWei('10.5', 'ether'))
+			.sendAndWaitForReceipt({from: source} as any)
+		await goldToken
+			.transfer(a1, toWei('0.5', 'ether'))
+			.sendAndWaitForReceipt({from: source} as any)
+		await goldToken
+			.transfer(a2, toWei('0.5', 'ether'))
+			.sendAndWaitForReceipt({from: source} as any)
+	})
 
-		const instance = await HelloContract.deployed()
+	it(`locking`, async () => {
+		const goldToken = await kit.contracts.getGoldToken()
+		const helloContract = await HelloContract.deployed()
 
 		// Allow HelloContract to transfer 10 CELO from `a0`, and call Lock function on it.
 		await goldToken
-			.increaseAllowance(instance.address, 10e18.toFixed(0))
+			.increaseAllowance(helloContract.address, toWei('10', 'ether'))
 			.sendAndWaitForReceipt({from: a0} as any)
-		await instance.Lock(10e18.toFixed(0), {from: a0})
+		await helloContract.Lock(toWei('10', 'ether'), {from: a0})
+
+		const balanceA0 = await goldToken.balanceOf(a0)
+		assert.isTrue(balanceA0.lt(toWei('0.5', 'ether')), `balance a0: ${balanceA0}`)
+	})
+
+	it (`unlocking`, async () => {
+		const goldToken = await kit.contracts.getGoldToken()
+		const lockedGold = await kit.contracts.getLockedGold()
+		const helloContract = await HelloContract.deployed()
 
 		// Anyone can call Unlock.
-		await instance.Unlock(3e18.toFixed(0), {from: a1})
-		await instance.Unlock(7e18.toFixed(0), {from: a2})
+		await helloContract.Unlock(toWei('3', 'ether'), {from: a1})
+		await helloContract.Unlock(toWei('7', 'ether'), {from: a2})
 
-		const pendings = await lockedGold.getPendingWithdrawals(instance.address)
+		const pendings = await lockedGold.getPendingWithdrawals(helloContract.address)
 		assert.lengthOf(pendings, 2)
-		assert.equal(pendings[0].value.toNumber(), 3e18)
-		assert.equal(pendings[1].value.toNumber(), 7e18)
+		assert.isTrue(pendings[0].value.eq(toWei('3', 'ether')))
+		assert.isTrue(pendings[1].value.eq(toWei('7', 'ether')))
 
 		// Make sure unlock period passes.
 		await increaseTime(kit.web3.currentProvider as Provider, 3 * 24 * 3600 + 1)
 
 		// Anyone can withdraw anything..., `a1` gets +7 CELO, `a2` gets +3 CELO.
-		await instance.Withdraw(1, {from: a1})
-		await instance.Withdraw(0, {from: a2})
+		await helloContract.Withdraw(1, {from: a1})
+		await helloContract.Withdraw(0, {from: a2})
 
-		const contractCELO = await goldToken.balanceOf(instance.address)
-		const contractLockedCELO = await lockedGold.getAccountTotalLockedGold(instance.address)
+		const contractCELO = await goldToken.balanceOf(helloContract.address)
+		const contractLockedCELO = await lockedGold.getAccountTotalLockedGold(helloContract.address)
 		assert.equal(contractCELO.toNumber(), 0)
 		assert.equal(contractLockedCELO.toNumber(), 0)
 
-		const finalBalanceA0 = await goldToken.balanceOf(a0)
-		const finalBalanceA1 = await goldToken.balanceOf(a1)
-		const finalBalanceA2 = await goldToken.balanceOf(a2)
-		assert.closeTo(finalBalanceA0.toNumber(), balanceA0.minus(10e18).toNumber(), 0.1e18) // Delta comes from Gas costs.
-		assert.closeTo(finalBalanceA1.toNumber(), balanceA1.plus(7e18).toNumber(), 0.1e18)
-		assert.closeTo(finalBalanceA2.toNumber(), balanceA2.plus(3e18).toNumber(), 0.1e18)
+		const balanceA1 = await goldToken.balanceOf(a1)
+		const balanceA2 = await goldToken.balanceOf(a2)
+		assert.isTrue(balanceA1.gt(toWei('7', 'ether')), `balance a1: ${balanceA1}`)
+		assert.isTrue(balanceA2.gt(toWei('3', 'ether')), `balance a2: ${balanceA2}`)
 	})
 })
 
